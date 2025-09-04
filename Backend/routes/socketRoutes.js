d42344888ad6b3cf021cd6bb10dd5f684f102a3d
Tanmayshukla25 <tanmayshukla252@gmail.com>
2025-09-04 15:24:33 +0530

    FixSyntaxError

--- Backend/routes/socketRoutes.js
+++ Backend/routes/socketRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Conversation from "../models/messageSchema.js";
-import { uploadMessageFile } from "../Middleware/uploadMessageFile.js";
+import { uploadMessageFile } from "../MiddleWare/uploadMessageFile.js";
 
 const router = express.Router();
 
